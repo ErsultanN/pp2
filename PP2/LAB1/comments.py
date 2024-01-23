687ba58b9a35144ebe841c8685fdeb9010412b21
ErsultanN <156774069+ErsultanN@users.noreply.github.com>
2024-01-23 15:47:57 +0600

    Add files via upload

---
+++ PP2/LAB1/comments.py
@@ -0,0 +1,12 @@
+#ex1
+print("HEllo, World!")
+# This is a comment
+
+#ex2
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!") 
---
+++ PP2/LAB1/syntax.py
@@ -0,0 +1,6 @@
+# Ex1
+print("Hello, World!") 
+
+# Ex2
+if 5 > 2:
+    print("YES")
