cccce1f2496136d56a6cf532dff8e3290bdc7d9f
Admin <ersultan1109@gmail.com>
2024-01-30 00:26:42 +0600

    LAB2

---
+++ PP2/LAB2/ifElse.py
@@ -0,0 +1,60 @@
+# Ex1
+a = 50
+b = 10
+if a > b:
+
+  print("Hello World")
+
+# Ex2
+a = 50
+b = 10
+if a != b:
+
+  print("Hello World")
+
+# Ex3
+  a = 50
+b = 10
+if a == b:
+
+  print("Yes")
+else:
+
+  print("No")
+
+# Ex4
+  
+a = 50
+b = 10
+if a == b:
+
+  print("1")
+elif a > b:
+
+  print("2")
+else:
+
+  print("3")
+
+# Ex5  
+if a == b and a == b:
+     print("Hello")
+
+# Ex6
+if a == b or a == b: 
+   print("Hello")
+
+# Ex7
+if 5 > 2:
+    print("YES")
+
+# Ex8
+a = 2
+b = 5
+print("YES") if a == b else print("NO")
+
+# Ex9
+a = 2
+b = 5
+print("YES") if a == b else print("NO")
+    
